5894d10c94de0baad69d49c5e22c7d641d09a747
Mckade Daines <mckadedaines1@gmail.com>
2022-04-22 12:40:01 -0600

    Portal Page progress

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,54 @@
+// SEQUENCE
+// variables and the Date object
+
 let date = new Date();
-console.log(date);
-
 let year = date.getFullYear();
-console.log(year);
 
+// Selecting element in HTML and DOM manipulation
 document.querySelector('h1').textContent = year;
 
 document.querySelector('div').innerHTML = '<h2>Subtitle</h2>';
 
 let newpara = document.createElement('p');
 newpara.textContent = 'This is my paragraph';
-document.querySelector('div').appendChild(newpara);
 
 document.querySelector('img').setAttribute('src', 'images/dog.jpg');
+document.querySelector('img').setAttribute('width', '400');
+
+document.querySelector("img").classList.add("add_border");
+
+// Funtions
+function addayaer(yr) {
+    console.log(yr);
+}
+
+addayaer(year);
+
+// SELECTION
+
+// if clause
+
+if (year == 2022) {
+    console.log("current year");
+}
+
+// REPETITION (loops)
+
+const myarray = ["CSE121b", "WDD230", "CIT111"];
+
+function makelist(item) {
+    document.querySelector("div").innerHTML += "<li>" +
+    item + "</li>"
+}
+
+myarray.forEach(makelist);
+
+// this is all you need with JS for Week 1
+
+// lastModified property that works with document
+
+let currentdate = document.lastModified;
+
+document.querySelector("#lastdate").textContent = currentdate;
+
+console.log(currentdate);
