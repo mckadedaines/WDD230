18f978726f31fa8d811848d80046626694fab212
Mckade Daines <mckadedaines1@gmail.com>
2022-04-25 12:31:52 -0600

    update

--- scripts/portal.js
+++ scripts/portal.js
@@ -1,5 +1,8 @@
+// let date = new Date();
+// let year = date.getFullYear();
+
+// document.querySelector("#year").textContent = year;
+
 let currentdate = document.lastModified;
-
 document.querySelector("#update").textContent = currentdate;
-
 console.log(currentdate);
