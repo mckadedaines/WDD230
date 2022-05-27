ac487204c2ed41dd6d2fa15dac9125917dc958b8
Mckade <mckadedaines1@gmail.com>
2022-05-27 14:11:43 -0600

    help

---
+++ Chamber/scripts/windchill.js
@@ -0,0 +1,24 @@
+// Calculates windchill temp
+
+let speed = 25;
+let temp = 10;
+
+function buildWC(speed, temp)
+{
+    let feelTemp = document.getElementById("feelTemp");
+
+    // Compute the windchill
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc);
+
+    // Round the answer down to integer
+    wc = Math.floor(wc);
+
+    // If chill is greater than temp, return the temp
+    wc = (wc > temp) ? temp : wc;
+
+    // Display the windchill
+    console.log(wc);
+    // wc = "Feels like '+wc+'F";
+    feelTemp.innerHTML = wc;
+}
