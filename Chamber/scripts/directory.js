03aa23470634b1720659e48020bfb882b2b8750f
Mckade Daines <mckadedaines1@gmail.com>
2022-06-21 12:19:55 -0600

    weather yupdates

--- Chamber/scripts/directory.js
+++ Chamber/scripts/directory.js
@@ -1,2 +1,34 @@
-const jsonData = 'json/data.json';
-console.log(jsonData);
+const requestURL = "https://mckadedaines.github.io/WDD230/Chamber/json/data.json";
+
+async function getCards(requestURL) {
+    const response = await fetch(requestURL);
+    if (response.ok){
+        const jsonObject = await response.json();
+        console.log(jsonObject);
+        const cards = jsonObject['directory'];
+        cards.forEach(displayBusiness);
+        // prophets.forEach(displayTable);
+    }
+};
+
+getCards(requestURL);
+
+function displayBusiness(cards) {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let dob = document.createElement('p');
+    let pob = document.createElement('p');
+    let img = document.createElement('img');
+
+    h2.textContent = cards.name;
+    // Stopped right here
+    dob.textContent = `${prophet.birthdate}`;
+    pob.textContent = `${prophet.birthplace}`;
+    img.setAttribute('src', prophet.imageurl);
+    img.setAttribute('alt', `${prophet.name} ${prophet.lastname} ${prophet.order}`);
+    card.appendChild(h2);
+    card.appendChild(dob);
+    card.appendChild(pob);
+    card.appendChild(img);
+    document.querySelector('.cards').appendChild(card);
+}
---
+++ In class/weather.js
@@ -0,0 +1,17 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5605242&appid=22569e3a1a6455138bbd985b0660d5fa";
+const getWeather = async () => {
+    const response = await fetch(apiURL);
+    jsObject = await response.json();
+    console.log(jsObject);
+
+    let f = (jsObject.main.temp - 273.15) * 1.8 + 32;
+    document.querySelector('#current-temp').textContent = f.toFixed(2);
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+  };
+getWeather();
