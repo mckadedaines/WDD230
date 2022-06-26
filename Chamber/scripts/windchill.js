93e875ac72e4214cb5a2aabb9d0dcdd6664500b1
Mckade <mckadedaines1@gmail.com>
2022-06-25 20:27:39 -0600

    forgot how to pull from an API

--- Chamber/scripts/windchill.js
+++ Chamber/scripts/windchill.js
@@ -11,7 +11,7 @@ const getWeather = async () => {
 
     const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
-    document.querySelector('#icon-src').textContent = iconsrc;
+    // document.querySelector('#icon-src').textContent = iconsrc;
     document.querySelector('#weathericon').setAttribute('src', iconsrc);
     document.querySelector('#weathericon').setAttribute('alt', desc);
     document.querySelector('figcaption').textContent = desc;
